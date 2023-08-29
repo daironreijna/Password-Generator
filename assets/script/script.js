1d610a45741f967d3213acf1091093911f7be3de
Dairon Reijna <dairon.reijna@gmail.com>
2023-08-29 23:37:00 +0100

    placed initial code within a function - getPasswordOptions()

--- assets/script/script.js
+++ assets/script/script.js
@@ -28,87 +28,214 @@ const lowerCasedCharacters = [
 let upperCasedCharacters = [
   'A',  'B',  'C',  'D',  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L', 'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T',  'U',  'V',  'W',  'X',  'Y',  'Z'];
 
-// Arrays for user input
+
+function getPasswordOptions() {
+  console.log("Hello, I am a function declaration called getPasswordOptions.");
+  console.log("-----------------------------------");
+  //!  =======================================
+  //!  PROMPTS FOR USER INPUT - allow user to indicate their password preference
+
+  //=======================================
+  // This will display a prompt asking the user to enter a number. The parseInt() function will then convert the string entered by the user to an integer. If the user enters a non-numeric value, isNaN() will return true, and  passwordLengthUserInput = passwordMinimumLength will be assigned to passwordLengthUserInput // 13
+  
+  //  Arrays for user input
+  //  =======================================
+
+  let passwordLengthUserInput = parseInt(prompt(`\n\t Let's set the password length \n\n Choose a number between 8 and 128 \n `));
+  
+  if (isNaN(passwordLengthUserInput)) {
+    passwordLengthUserInput = 13; // passwords will be 13 characters if user inputs a wrong character.
+    alert("The character inputted is not a number.\n The password length has been set to 13.")
+  
+  //TODO check that whatever is inputted is also between 8 and 128.
+  
+  } else if (passwordLengthUserInput < 8 || passwordLengthUserInput > 128) {
+    passwordLengthUserInput = 13;
+    alert("The number inputted was not between 8 and 128.\n The password length has been set to 13.")
+  };
+  
+  //=======================================
+  
+  let passwordCapitalLetters = confirm(`Now, please indicate if you want capital letters to be included: \n 'OK' = Yes, select 'Cancel' for No`);
+  
+  //=======================================
+
+  let passwordNumbers = confirm(`Now, please indicate if you want numbers to be included: \n 'OK' = Yes, select 'Cancel' for No`);
+  
+  //=======================================
+  
+  let passwordSpecialCharacters = confirm(`Now, please indicate if you want special characters to be included: \n 'OK' = Yes, select 'Cancel' for No`);
+  
+  //!=======================================
+  //!  Code to determine how password Array will be populated with characters based on user input
+
+  if (passwordCapitalLetters && passwordNumbers && passwordSpecialCharacters) {
+    alert(`You have opted to have a password with Capital Letters, Numbers and Special Characters \n`);
+    passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, numericCharacters, specialCharacters);
+
+    //====================================
+
+  } else if (!passwordCapitalLetters && !passwordNumbers && !passwordSpecialCharacters) {
+    alert(`You have opted to not have any Capital Letters, Numbers or Special Characters in the generated password \n`);
+    passwordArray = lowerCasedCharacters;
+
+    //====================================
+
+  } else if (passwordCapitalLetters && !passwordNumbers && passwordSpecialCharacters) {
+    alert(`You have opted to have a password with Capital Letters and Special Characters but No Numbers \n`);
+    passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, specialCharacters);
+
+    //====================================
+
+  } else if (passwordCapitalLetters && passwordNumbers && !passwordSpecialCharacters) {
+    alert(`You have opted to have Numbers and Capital Letters but no Special Characters password \n`);
+    passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, numericCharacters);
+    
+    // ===================================
+
+  } else if (!passwordCapitalLetters && !passwordNumbers && passwordSpecialCharacters) {
+    alert(`You have opted to have a password with Special Characters but No Numbers or Capital Letters\n`);
+    passwordArray = lowerCasedCharacters.concat(specialCharacters);
+
+    // ===================================
+
+  } else if (!passwordCapitalLetters && passwordNumbers && !passwordSpecialCharacters) {
+    alert(`You have opted to have Numbers but no Capital Letters or Special Characters set within the generated password \n`);
+    passwordArray = lowerCasedCharacters.concat(numericCharacters);
+    
+    // ===================================
+
+  }  else if (passwordCapitalLetters && !passwordNumbers && !passwordSpecialCharacters) {
+    alert(`You have opted to have a password generated with Capital Letters but without Numbers or Special Characters \n`);
+    passwordArray = lowerCasedCharacters.concat(upperCasedCharacters);
+  } 
+  // END OF CODE TO POPULATE DATA INTO PASSWORD ARRAY
+  
+  // Return stops the execution of a function
+  return;
+}
+
+// Function Execution
 // =======================================================================================
 
-const passwordMinimumLength = 13; // passwords will always be 13 if user inputs a wrong character.
-let passwordLengthUserInput;
-let passwordCapitalLetters;
-let passwordNumbers;
-let passwordSpecialCharacters;
-let passwordArray;
+getPasswordOptions()
 
-//!=======================================================================================
-//!  PROMPTS FOR USER INPUT - allow user to indicate their password preference
 
-//========================================================================================
-// This will display a prompt asking the user to enter a number. The parseInt() function will then convert the string entered by the user to an integer. If the user enters a non-numeric value, isNaN() will return true, and  passwordLengthUserInput = passwordMinimumLength will be assigned to passwordLengthUserInput // 13
+// //!=======================================================================================
+// //! 
 
-passwordLengthUserInput = parseInt(prompt(`\n\t Let's set the password length \n\n Choose a number between 8 and 128 \n `));
+// let index = passwordArray.length
+// let randNum = Math.ceil((Math.random()*index))
 
-if (isNaN(passwordLengthUserInput)) {
-  passwordLengthUserInput = passwordMinimumLength;
-  alert("The character inputted is not a number.\n The password length has been set to 13.")
+// console.log(randNum)
 
-//TODO check that whatever is inputted is also between 8 and 128.
+// console.log(array)
 
-} else if (passwordLengthUserInput < 8 || passwordLengthUserInput > 128) {
-  alert("The number inputted was not between 8 and 128.\n The password length has been set to 13.")
-};
+// let index3 = passwordLengthUserInput
+// let array3 = Math.ceil(Math.random()*index3)
+// console.log(array3)
 
-//========================================================================================
+// arr = 1.75 // ensure that the math random is always calculating a number, trouble if user does not input any character, what will happen with the random element of the code?
 
-passwordCapitalLetters = confirm(`Now, please indicate if you want capital letters to be included: \n 'OK' = Yes, select 'Cancel' for No`);
+// math.ceil(math.random * arr += passwordUserInput.length)
 
-//========================================================================================
 
-passwordNumbers = confirm(`Now, please indicate if you want numbers to be included: \n 'OK' = Yes, select 'Cancel' for No`);
+// // Check that UserInput is a number
 
-//========================================================================================
+// function checkIndex() {
+//   for (let i=0; i< passwordLengthUserInput.length; i++) {
+//       if (passwordLengthUserInput < 8 || passwordLengthUserInput > 128) {
+//           console.log( ` ${passwordLengthUserInput} is outside of bounds`)
+//       } else if (passwordLengthUserInput => 8 || passwordLengthUserInput <= 128){
+//           console.log(` ${passwordLengthUserInput } is between 8 and 128`)
+//       // i think that the password generator needs a statement to exclude non-numerical values. I intend for the code to default to 13 characters. 
+//       } else {
+//         console.log( ` ${passwordLengthUserInput} is outside of bounds2`)
+//       } 
+//   }
+// }
+// // FUNCTION CALLS (Execution) 
+// // =======================================================================================
 
-passwordSpecialCharacters = confirm(`Now, please indicate if you want special characters to be included: \n 'OK' = Yes, select 'Cancel' for No`);
+// checkIndex();
 
-//!=======================================================================================
-//!  Code to determine how password Array will be populated with characters based on user input
 
-if (passwordCapitalLetters && passwordNumbers && passwordSpecialCharacters) {
-  alert(`You have opted to have a password with Capital Letters, Numbers and Special Characters \n`);
-  passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, numericCharacters, specialCharacters);
 
-  //====================================
+// // Function for getting a random element from an array
+// // =======================================================================================
 
-} else if (!passwordCapitalLetters && !passwordNumbers && !passwordSpecialCharacters) {
-  alert(`You have opted to not have any Capital Letters, Numbers or Special Characters in the generated password \n`);
-  passwordArray = lowerCasedCharacters;
 
-  //====================================
+// // function getRandom(arr) {
+// //   console.log("Hello, I am a function declaration called getRandom.");
+// //   console.log("-----------------------------------");
+// //   // Return stops the execution of a function
+// //   return math.ceil(math.random() * passwordLengthUserInput);
+// // }
 
-} else if (passwordCapitalLetters && !passwordNumbers && passwordSpecialCharacters) {
-  alert(`You have opted to have a password with Capital Letters and Special Characters but No Numbers \n`);
-  passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, specialCharacters);
+// //arr = 1.75 // ensure that the math random is always calculating a number, trouble if user does not input any character, what will happen with the random element of the code?
 
-  //====================================
+// let math = 0;
 
-} else if (passwordCapitalLetters && passwordNumbers && !passwordSpecialCharacters) {
-  alert(`You have opted to have Numbers and Capital Letters but no Special Characters password \n`);
-  passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, numericCharacters);
-  
-  // ===================================
+// /*arr* += */
 
-} else if (!passwordCapitalLetters && !passwordNumbers && passwordSpecialCharacters) {
-  alert(`You have opted to have a password with Special Characters but No Numbers or Capital Letters\n`);
-  passwordArray = lowerCasedCharacters.concat(specialCharacters);
+// // console.log(math.ceil(math.random() * passwordLengthUserInput[0]))
 
-  // ===================================
 
-} else if (!passwordCapitalLetters && passwordNumbers && !passwordSpecialCharacters) {
-  alert(`You have opted to have Numbers but no Capital Letters or Special Characters set within the generated password \n`);
-  passwordArray = lowerCasedCharacters.concat(numericCharacters);
-  
-  // ===================================
 
-}  else if (passwordCapitalLetters && !passwordNumbers && !passwordSpecialCharacters) {
-  alert(`You have opted to have a password generated with Capital Letters but without Numbers or Special Characters \n`);
-  passwordArray = lowerCasedCharacters.concat(upperCasedCharacters);
-} 
-// END OF CODE TO POPULATE DATA INTO PASSWORD ARRAY
+// // Function Execution
+// // =======================================================================================
+// getRandom()
+
+
+// // Function to generate password with user input
+// // =======================================================================================
+
+// alert(`Your password has been generated \r ${generatePassword}`) // not sure this is correct.
+
+// function generatePassword() {
+//   console.log("Hello, I am a function declaration called generatePassword.");
+//   console.log("-----------------------------------");
+//   // Return stops the execution of a function
+//   return;
+// }
+
+// console.log(passwordArray.find(function(i) {
+//   let passwordGenerate = passwordGenerate.concat(passwordArray[i])
+//   return passwordGenerate
+// }))
+// // Function Execution
+// // =======================================================================================
+// generatePassword()
+
+// // Get references to the #generate element
+// // =======================================================================================
+
+// var generateBtn = document.querySelector('#generate');
+
+// // Write password to the #password input
+// // =======================================================================================
+
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector('#password');
+
+//   passwordText.value = password;
+//   console.log("Hello, I am a function declaration called writePassword.");
+//   console.log("-----------------------------------");
+//   // Return stops the execution of a function
+//   return;
+// }
+
+
+// // Function Execution
+// // =======================================================================================
+
+// writePassword()
+
+// // Add event listener to generate button
+// // =======================================================================================
+
+// generateBtn.addEventListener('click', writePassword);
+
+
+// */
