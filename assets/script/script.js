45b13f771eec2f4d579b43209cbe4adf48c0313a
Dairon Reijna <dairon.reijna@gmail.com>
2023-08-29 18:04:47 +0100

    revisited code to ensure logic makes sense, user input with validation, next step to generatePassword

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,202 +1,114 @@
 // start with clearing the console.
 // =======================================================================================
+
 console.clear()
 
+//!=======================================================================================
+//! INITIAL SETUP OF ARRAYS
 
 // Array of special characters to be included in password
 // =======================================================================================
+
 const specialCharacters = [
-  '@',   '%',   '+',  '\\', '/', "'", '!',  '#',  '$',  '^',
-  '?',   ':',   ',',  ')',  '(', '}', '{',  ']',  '[',   '~',
-  '-',   '_',   '.'
-];
+  '@',   '%',   '+',  '\\', '/', "'", '!',  '#',  '$',  '^', '?',   ':',   ',',  ')',  '(', '}', '{',  ']',  '[',   '~', '-',   '_',   '.' ];
 
 // Array of numeric characters to be included in password
 // =======================================================================================
-const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
 // =======================================================================================
 const lowerCasedCharacters = [
-  'a',  'b',  'c',  'd',  'e',  'f',  'g',  'h',  'i',  'j',  'k',  'l',
-  'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x',
-  'y',  'z'
-];
+  'a',  'b',  'c',  'd',  'e',  'f',  'g',  'h',  'i',  'j',  'k',  'l', 'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x', 'y',  'z'];
 
 // Array of uppercase characters to be included in password
 // =======================================================================================
-let upperCasedCharacters = [
-  'A',  'B',  'C',  'D',  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L',
-  'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T',  'U',  'V',  'W',  'X',
-  'Y',  'Z'
-];
 
-// Setup of variable containers
+let upperCasedCharacters = [
+  'A',  'B',  'C',  'D',  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L', 'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T',  'U',  'V',  'W',  'X',  'Y',  'Z'];
+
+// Arrays for user input
+// =======================================================================================
 
 const passwordMinimumLength = 13; // passwords will always be 13 if user inputs a wrong character.
-let passwordLengthUserInput = 13;
+let passwordLengthUserInput;
 let passwordCapitalLetters;
 let passwordNumbers;
 let passwordSpecialCharacters;
 let passwordArray;
 
-// Function to prompt user for password options
-// =======================================================================================
+//!=======================================================================================
+//!  PROMPTS FOR USER INPUT - allow user to indicate their password preference
 
-passwordLengthUserInput = prompt(`Let's set the password length \n\n Choose a number between 8 and 128 \n `);
+//========================================================================================
+// This will display a prompt asking the user to enter a number. The parseInt() function will then convert the string entered by the user to an integer. If the user enters a non-numeric value, isNaN() will return true, and  passwordLengthUserInput = passwordMinimumLength will be assigned to passwordLengthUserInput // 13
 
-console.log(passwordLengthUserInput);
+passwordLengthUserInput = parseInt(prompt(`\n\t Let's set the password length \n\n Choose a number between 8 and 128 \n `));
 
-// Creates an array of the values of the object
-console.log(`object values ${Object.values(passwordLengthUserInput)}`)
+if (isNaN(passwordLengthUserInput)) {
+  passwordLengthUserInput = passwordMinimumLength;
+  alert("The character inputted is not a number.\n The password length has been set to 13.")
 
-console.log(`object typeof ${typeof Object.values(passwordLengthUserInput)}`)
+//TODO check that whatever is inputted is also between 8 and 128.
 
-console.log(`input typeof ${typeof passwordLengthUserInput}`)
+} else if (passwordLengthUserInput < 8 || passwordLengthUserInput > 128) {
+  alert("The number inputted was not between 8 and 128.\n The password length has been set to 13.")
+};
 
-// if (passwordLengthUserInput != "string") {
+//========================================================================================
 
-// } else {
+passwordCapitalLetters = confirm(`Now, please indicate if you want capital letters to be included: \n 'OK' = Yes, select 'Cancel' for No`);
 
-// }
+//========================================================================================
 
-passwordCapitalLetters = confirm(`Now, please indicate if you want capital letters to be included: \n 'OK' = Yes, select 'Cancel' for No
-`);
+passwordNumbers = confirm(`Now, please indicate if you want numbers to be included: \n 'OK' = Yes, select 'Cancel' for No`);
 
-console.log(passwordCapitalLetters);
+//========================================================================================
 
-passwordNumbers = confirm(`Now, please indicate if you want numbers to be included: \n 'OK' = Yes, select 'Cancel' for No
-`);
+passwordSpecialCharacters = confirm(`Now, please indicate if you want special characters to be included: \n 'OK' = Yes, select 'Cancel' for No`);
 
-console.log(passwordNumbers)
-
-passwordSpecialCharacters = confirm(`Now, please indicate if you want special characters to be included: \n 'OK' = Yes, select 'Cancel' for No
-`);
-
-console.log(passwordSpecialCharacters)
+//!=======================================================================================
+//!  Code to determine how password Array will be populated with characters based on user input
 
 if (passwordCapitalLetters && passwordNumbers && passwordSpecialCharacters) {
-  console.log(`You have opted to have a password with Capital Letters, Numbers and Special Characters \n`);
+  alert(`You have opted to have a password with Capital Letters, Numbers and Special Characters \n`);
   passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, numericCharacters, specialCharacters);
+
+  //====================================
+
 } else if (!passwordCapitalLetters && !passwordNumbers && !passwordSpecialCharacters) {
-  console.log(`You have opted to not have any Capital Letters, Numbers or Special Characters in the generated password \n`);
+  alert(`You have opted to not have any Capital Letters, Numbers or Special Characters in the generated password \n`);
   passwordArray = lowerCasedCharacters;
+
+  //====================================
+
 } else if (passwordCapitalLetters && !passwordNumbers && passwordSpecialCharacters) {
-  console.log(`You have opted to have a password with Capital Letters and Special Characters but No Numbers \n`);
+  alert(`You have opted to have a password with Capital Letters and Special Characters but No Numbers \n`);
   passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, specialCharacters);
+
+  //====================================
+
 } else if (passwordCapitalLetters && passwordNumbers && !passwordSpecialCharacters) {
-  console.log(`You have opted to have Numbers and Capital Letters but no Special Characters password \n`);
+  alert(`You have opted to have Numbers and Capital Letters but no Special Characters password \n`);
   passwordArray = lowerCasedCharacters.concat(upperCasedCharacters, numericCharacters);
+  
+  // ===================================
+
 } else if (!passwordCapitalLetters && !passwordNumbers && passwordSpecialCharacters) {
-  console.log(`You have opted to have a password with Special Characters but No Numbers or Capital Letters\n`);
+  alert(`You have opted to have a password with Special Characters but No Numbers or Capital Letters\n`);
   passwordArray = lowerCasedCharacters.concat(specialCharacters);
+
+  // ===================================
+
 } else if (!passwordCapitalLetters && passwordNumbers && !passwordSpecialCharacters) {
-  console.log(`You have opted to have Numbers but no Capital Letters or Special Characters set within the generated password \n`);
+  alert(`You have opted to have Numbers but no Capital Letters or Special Characters set within the generated password \n`);
   passwordArray = lowerCasedCharacters.concat(numericCharacters);
+  
+  // ===================================
+
 }  else if (passwordCapitalLetters && !passwordNumbers && !passwordSpecialCharacters) {
-  console.log(`You have opted to have a password generated with Capital Letters but without Numbers or Special Characters \n`);
+  alert(`You have opted to have a password generated with Capital Letters but without Numbers or Special Characters \n`);
   passwordArray = lowerCasedCharacters.concat(upperCasedCharacters);
 } 
-
-console.log(passwordArray)
-
-
-function getPasswordOptions() {
-  console.log("Hello, I am a function declaration called getPasswordOptions.");
-  console.log("-----------------------------------");
-  // Return stops the execution of a function
-  return;
-}
-
-// Function Execution
-// =======================================================================================
-
-getPasswordOptions()
-
-// Check that UserInput is a number
-
-function checkIndex() {
-  for (let i=0; i< passwordLengthUserInput.length; i++) {
-      if (passwordLengthUserInput < 8 || passwordLengthUserInput > 128) {
-          console.log( ` ${passwordLengthUserInput} is outside of bounds`)
-      } else if (passwordLengthUserInput => 8 || passwordLengthUserInput <= 128){
-          console.log(` ${passwordLengthUserInput } is between 8 and 128`)
-      // i think that the password generator needs a statement to exclude non-numerical values. I intend for the code to default to 13 characters. 
-      } else {
-        console.log( ` ${passwordLengthUserInput} is outside of bounds2`)
-      } 
-  }
-}
-// FUNCTION CALLS (Execution) 
-// =======================================================================================
-
-checkIndex();
-
-
-
-// Function for getting a random element from an array
-// =======================================================================================
-
-
-// function getRandom(arr) {
-//   console.log("Hello, I am a function declaration called getRandom.");
-//   console.log("-----------------------------------");
-//   // Return stops the execution of a function
-//   return math.ceil(math.random() * passwordLengthUserInput);
-// }
-
-// Function Execution
-// =======================================================================================
-getRandom()
-
-
-// Function to generate password with user input
-// =======================================================================================
-
-alert(`Your password has been generated \n \n ${generatePassword}`) // not sure this is correct.
-
-function generatePassword() {
-  console.log("Hello, I am a function declaration called generatePassword.");
-  console.log("-----------------------------------");
-  // Return stops the execution of a function
-  return;
-}
-
-console.log(passwordArray.find(function(i) {
-  return 
-  let passwordGenerate = passwordGenerate.concat(passwordArray[i])
-}))
-// Function Execution
-// =======================================================================================
-generatePassword()
-
-// Get references to the #generate element
-// =======================================================================================
-
-var generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-// =======================================================================================
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-  console.log("Hello, I am a function declaration called writePassword.");
-  console.log("-----------------------------------");
-  // Return stops the execution of a function
-  return;
-}
-
-
-// Function Execution
-// =======================================================================================
-
-writePassword()
-
-// Add event listener to generate button
-// =======================================================================================
-
-generateBtn.addEventListener('click', writePassword);
+// END OF CODE TO POPULATE DATA INTO PASSWORD ARRAY
